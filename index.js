1a73fcf1b1c4a67df97580165d7418c58d44ab8d
emilieg <emilie.gerber@hotmail.com>
2016-06-05 23:57:24 -0700

    fixed app.use dirname '/public'

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var app = express();
 var path = require('path');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(__dirname + 'public'));
-
+app.use(express.static(__dirname + '/public'));
+// app.use("/node_modules", express.static(__dirname + 'public'));
 
 app.get('/', function(req, res) {
   console.log("hello");
